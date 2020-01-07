81ccff7f993e2d924689feffac1dc5ce89f974d8
moshenglei <moshenglei@zhongan.com>
2020-01-07 20:51:22 +0800

    a

--- src/main/java/com/mosl/FooTest.java
+++ src/main/java/com/mosl/FooTest.java
@@ -1,5 +1,6 @@
 package com.mosl;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,25 +8,50 @@ public class FooTest {
 
     private Lock lock = new ReentrantLock();
 
-    public FooTest() {
+    private Condition a = lock.newCondition();
 
+    private Condition b = lock.newCondition();
+
+    private Condition c = lock.newCondition();
+
+    private volatile int flag = 0;
+
+    public FooTest() {
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-
+        lock.lock();//获取到锁
+        if (flag != 0) {
+            a.await();
+        }
         // printFirst.run() outputs "first". Do not change or remove this line.
         printFirst.run();
+        flag = 1;
+        b.signal();
+        lock.unlock();
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-
+        lock.lock();//获取到锁
+        if (flag != 1) {
+            b.await();
+        }
         // printSecond.run() outputs "second". Do not change or remove this line.
         printSecond.run();
+        flag = 2;
+        c.signal(); //唤醒C
+        lock.unlock();
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-
+        lock.lock();
+        if (flag != 2) {
+            c.await();
+        }
         // printThird.run() outputs "third". Do not change or remove this line.
         printThird.run();
+        flag = 0;
+        a.signal(); //唤醒A
+        lock.unlock();
     }
 }
---
+++ src/main/java/com/mosl/SynchronizedFooTest.java
@@ -0,0 +1,69 @@
+package com.mosl;
+
+public class SynchronizedFooTest {
+
+    private int flag = 0;
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (this) {
+            if (flag != 0) {
+                this.wait();//等待
+            }
+            //如果flag == 0
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            flag = 1;
+            this.notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (this) {
+            if (flag != 1) {
+                this.wait();
+            }
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            flag = 2;
+            this.notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (this) {
+            if (flag != 2) {
+                this.wait();
+            }
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+            flag = 0;
+            this.notifyAll();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SynchronizedFooTest fooTest = new SynchronizedFooTest();
+        new Thread(() -> {
+            try {
+                fooTest.first(() -> { System.out.print("one");});
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                fooTest.second(() -> { System.out.print("two");});
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                fooTest.third(() -> { System.out.print("three");});
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+}
